598c2974e2efc2215d8968d03aa861cbcc31ecb8
Vivi <vivi110099@outlook.com>
2024-12-07 16:21:53 +0300

    Update minik.js

--- minik.js
+++ minik.js
@@ -47,8 +47,17 @@ client
 
 
 
+    // Mongo DB 
+    const mongoose = require('mongoose');
 
-
+    mongoose.connect('mongodb://localhost:27017/BotDatabase', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('MongoDB bağlantısı başarılı!');
+    }).catch((err) => {
+        console.error('MongoDB bağlantı hatası:', err);
+    });    
 
 
 
